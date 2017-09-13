e99f57f7d083a25cf98f7c8436106c83f21fe55b
Tom Wilczak <tom.wilczak@gmail.com>
2017-09-13 18:17:46 -0500

    Move message controls functionality into message composer

--- src/app/message-composer/message-composer.component.ts
+++ src/app/message-composer/message-composer.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { MailMessage } from "../mail-message";
+import { Mailbox } from "../mailbox.service";
+import { MailService } from "../mail.service";
+
 
 @Component({
   selector: 'message-composer',
@@ -10,13 +16,41 @@ export class MessageComposerComponent implements OnInit {
 
   messageForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private route: ActivatedRoute, private router: Router,
+              private mailbox: Mailbox, private mailService: MailService) { }
 
   ngOnInit() {
     this.messageForm = this.formBuilder.group({
       recipient: ['', Validators.required],
       subject: ['', Validators.required],
       body: ['', Validators.required]
+    });
+  }
+
+  cancel() {
+    let mailbox: string = this.route.snapshot.parent.url[0].path;
+    this.router.navigateByUrl(mailbox);
+  }
+
+  sendDisabled() {
+    return this.messageForm.status !== 'VALID';
+  }
+
+  send() {
+    let dateSent = new Date();
+    let message = new MailMessage();
+    message.sender = 'Me';
+    message.recipient = this.messageForm.value.recipient;
+    message.subject = this.messageForm.value.subject;
+    message.setDateSent(dateSent);
+
+    this.mailService.sendMessage(message).subscribe((message) => {
+      if(this.mailbox.name === 'outbox') {
+        this.mailbox.messages.push(message);
+        this.router.navigateByUrl(`outbox/view/${message.id}`);
+      } else {
+        this.router.navigateByUrl(`inbox`);
+      }
     });
   }
 }
