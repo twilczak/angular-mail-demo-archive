f7d8f0b24ced53f3ca9e58f371bbb26edf50c442
Tom Wilczak <tom.wilczak@gmail.com>
2017-09-13 18:00:47 -0500

    Move delete functionatlity to message-reader component

--- src/app/message-reader/message-reader.component.ts
+++ src/app/message-reader/message-reader.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
 
 import { MailMessage } from "../mail-message";
-import { ActivatedRoute } from "@angular/router";
+import { Mailbox } from "../mailbox.service";
+import { MailService } from "../mail.service";
 
 @Component({
   selector: 'message-reader',
@@ -12,11 +14,25 @@ export class MessageReaderComponent implements OnInit {
 
   message: MailMessage;
 
-  constructor(private route: ActivatedRoute ) { }
+  constructor(private route: ActivatedRoute, private mailService: MailService, private mailbox: Mailbox, private router: Router) { }
 
   ngOnInit() {
     this.route.data.subscribe( (data: { message: MailMessage}) => {
       this.message = data.message;
+    });
+  }
+
+  deleteDisabled() {
+    return !this.message;
+  }
+
+  deleteMessage() {
+    let mailbox: string = this.route.snapshot.parent.url[0].path;
+    let id : string = this.message.id;
+
+    this.mailService.deleteMessage(mailbox, id).subscribe(() => {
+      this.mailbox.delete(id);
+      this.router.navigateByUrl(mailbox);
     });
   }
 }
